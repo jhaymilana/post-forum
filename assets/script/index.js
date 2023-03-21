250d806bbfa47d45b86f7ceb52690f98e61991ac
Jayvee Milana <jmmilana12@gmail.com>
2023-03-21 12:45:48 -0500

    added posts

--- assets/script/index.js
+++ assets/script/index.js
@@ -8,3 +8,27 @@ Fakebook
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 'use strict';
+
+let post = document.querySelector('.post');
+let storage = document.querySelector('.storage');
+let text = document.querySelector('.text');
+let image = document.querySelector('.image');
+
+function createPost() {
+  let newPost = document.createElement('div');
+  let postText = document.createElement('p');
+  let imageFile = document.createElement('img');
+
+  newPost.className = `posts`;
+
+  postText.innerHTML = `${text.value}`;
+  imageFile.src = `${image.value}`;
+
+  storage.appendChild(newPost);
+  newPost.appendChild(postText);
+  newPost.appendChild(imageFile);
+}
+
+post.addEventListener('click', function() {
+  createPost();
+});
