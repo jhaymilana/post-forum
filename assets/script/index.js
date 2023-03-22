f76531d2069b6471f5ca2ccf6101495674b2fc26
Jayvee Milana <jmmilana12@gmail.com>
2023-03-21 22:45:17 -0500

    classes created

--- assets/script/index.js
+++ assets/script/index.js
@@ -13,6 +13,8 @@ let post = document.querySelector('.post');
 let storage = document.querySelector('.storage');
 let text = document.querySelector('.text');
 let image = document.querySelector('.image');
+let user = document.querySelector('.user');
+let userInfo = document.querySelector('.user-info');
 
 function createPost() {
   let newPost = document.createElement('div');
@@ -33,3 +35,34 @@ post.addEventListener('click', function() {
   createPost();
   text.value = '';
 });
+
+user.addEventListener('click', function() {
+  userInfo.classList.toggle('hidden');
+});
+
+class User {
+  #id;
+  #name;
+  #userName;
+  #email;
+
+  constructor (id, name, userName, email) {
+    this.#id = id;
+    this.#name = name;
+    this.#userName = userName;
+    this.#email = email;
+  }
+}
+
+class Subscriber extends User {
+  #pages;
+  #groups;
+  #canMonotize;
+
+  constructor (id, name, userName, email, pages, groups, canMonotize) {
+    super(id, name, userName, email);
+    this.#pages = pages;
+    this.#groups = groups;
+    this.#canMonotize = canMonotize
+  }
+}
